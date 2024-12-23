0a2b8da24a3f2e45a05a9e128a6e50b2e3a581a5
TienDoan <doantien2001@gmail.com>
2024-12-23 10:36:49 +0700

    change text content

--- AdsApp/ContentView.swift
+++ AdsApp/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Github")
+            Text("TiendoanBranchCheckjfjghjg")
+            Text("TiendoanBranchCheckjfjghjg")
         }
         .padding()
     }
