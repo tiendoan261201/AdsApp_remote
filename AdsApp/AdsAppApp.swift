364ab1cf6c5bb6e3101e7079a8023e2db34e0551
TienDoan <doantien2001@gmail.com>
2024-12-23 10:03:10 +0700

    Initial Commit

---
+++ AdsApp/AdsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdsAppApp.swift
+//  AdsApp
+//
+//  Created by Tiến Đoàn on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AdsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
